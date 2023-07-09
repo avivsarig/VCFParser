46bed4e330adb00d7e4dd386bf6ea2c3d86a63e5
AvivSarig <avivs15@gmail.com>
2023-07-09 20:26:43 +0300

    add typer documentation

--- main.py
+++ main.py
@@ -15,11 +15,21 @@ from parse_functions.columns import parse_columns
 from parse_functions.line import parse_data_line, unparse_data_line
 from parse_functions.filter import is_filtered
 
-app = typer.Typer()
-
+app = typer.Typer(help="A VCF file parser and filter. It handles compressed VCF files from Amazon S3, parses and filters them, then outputs the results into separate files for father, mother, and proband.")
 
 @app.command()
-def filter(limit: int, start: int = 0, end: int = -1, minDP: int = -1, deNovo: bool = False):
+def process(
+    limit: int = typer.Option(..., help="The maximum number of variants to process."), 
+    start: int = typer.Option(0, help="The start position for filtering."), 
+    end: int = typer.Option(-1, help="The end position for filtering."), 
+    minDP: int = typer.Option(-1, help="The minimum depth to filter by."), 
+    deNovo: bool = typer.Option(False, help="Whether to include de novo mutations.")
+):
+    """
+    Download, parse, filter and output a VCF file.
+
+    This command takes a compressed VCF file from S3, parses it, applies filters based on given arguments, and outputs the results into separate files for father, mother, and proband.
+    """
     if limit >= 10:
         print("Limit must be smaller than 10")
         return False
@@ -78,6 +88,11 @@ def filter(limit: int, start: int = 0, end: int = -1, minDP: int = -1, deNovo: b
 
 @app.command()
 def clear_cache():
+    """
+    Clears the Gene Cache.
+
+    Use this command to reset the contents of the gene cache. This action cannot be undone.
+    """
     confirmation = input(
         f"Are you sure you want to reset the contents of the Gene Cache? This cannot be undone.\nEnter (yes/no): "
     )
@@ -91,6 +106,11 @@ def clear_cache():
 
 @app.command()
 def clear_output():
+    """
+    Clears the output folder.
+
+    Use this command to reset the contents of the output folder. This action cannot be undone.
+    """
     confirmation = input(
         f"Are you sure you want to reset the contents of the output folder? This cannot be undone.\nEnter (yes/no): "
     )
